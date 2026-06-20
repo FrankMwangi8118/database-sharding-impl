e972bc2381682c586bd5e9824d844558048a2063
frank mwangi <frankmwangi8118@gmail.com>
2026-06-20 20:21:23 +0300

    feat: initial commit feat: set up migration script and docker compose

---
+++ src/main/java/com/codify/shardingimpl/ShardingImplApplication.java
@@ -0,0 +1,13 @@
+package com.codify.shardingimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingImplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codify/shardingimpl/ShardingImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codify.shardingimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
